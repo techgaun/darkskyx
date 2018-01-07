41e26b79ae91fd777f125cba9829466aed2146c0
Bertrand Dubaut <bdubaut@gmail.com>
2018-01-07 13:17:56 +0100

    Explicitely add poison to the package's dependencies. closes #2.

--- mix.exs
+++ mix.exs
@@ -32,6 +32,7 @@ defmodule Darkskyx.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [
+      {:poison, "~> 3.1"},
       {:httpoison, "~> 0.8 or ~> 0.9"},
       {:earmark, "~> 0.1", only: :dev},
       {:ex_doc, "~> 0.11", only: :dev},
